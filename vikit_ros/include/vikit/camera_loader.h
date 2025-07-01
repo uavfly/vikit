f262533d04d99282ef63b2b3c8900eac71e6eafb
uavfly <pzhy521@163.com>
2025-07-01 22:02:49 +0800

    Fix bugs.

--- vikit_ros/include/vikit/camera_loader.h
+++ vikit_ros/include/vikit/camera_loader.h
@@ -21,72 +21,144 @@ namespace vk {
 namespace camera_loader {
 
 /// Load from ROS Namespace
-bool loadFromRosNs(const std::string& ns, vk::AbstractCamera*& cam)
+bool loadFromRosNs(rclcpp::Node::SharedPtr nh, const std::string& ns, vk::AbstractCamera*& cam)
 {
   bool res = true;
-  std::string cam_model(getParam<std::string>(ns+"/cam_model"));
-  if(cam_model == "Ocam")
-  {
-    cam = new vk::OmniCamera(getParam<std::string>(ns+"/cam_calib_file", ""));
+  std::string cam_model;
+  if (!nh->get_parameter(ns + ".cam_model", cam_model)) {
+    RCLCPP_ERROR(nh->get_logger(), "Camera model parameter not found: %s.cam_model", ns.c_str());
+    return false;
   }
-  else if(cam_model == "Pinhole")
+
+  if (cam_model == "Ocam") {
+    std::string calib_file;
+    nh->get_parameter(ns + ".cam_calib_file", calib_file);
+    cam = new vk::OmniCamera(calib_file);
+  }
+  else if (cam_model == "Pinhole") 
   {
+    int cam_width = 640;
+    int cam_height = 480;
+    double scale = 1.0;
+    double cam_fx = 0.0;
+    double cam_fy = 0.0;
+    double cam_cx = 0.0;
+    double cam_cy = 0.0;
+    double cam_d0 = 0.0;
+    double cam_d1 = 0.0;
+    double cam_d2 = 0.0;
+    double cam_d3 = 0.0;
+
+    nh->get_parameter(ns + ".cam_width", cam_width);
+    nh->get_parameter(ns + ".cam_height", cam_height);
+    nh->get_parameter(ns + ".scale", scale);
+    nh->get_parameter(ns + ".cam_fx", cam_fx);
+    nh->get_parameter(ns + ".cam_fy", cam_fy);
+    nh->get_parameter(ns + ".cam_cx", cam_cx);
+    nh->get_parameter(ns + ".cam_cy", cam_cy);
+    nh->get_parameter(ns + ".cam_d0", cam_d0);
+    nh->get_parameter(ns + ".cam_d1", cam_d1);
+    nh->get_parameter(ns + ".cam_d2", cam_d2);
+    nh->get_parameter(ns + ".cam_d3", cam_d3);
+
     cam = new vk::PinholeCamera(
-        getParam<int>(ns+"/cam_width"),
-        getParam<int>(ns+"/cam_height"),
-        getParam<double>(ns+"/scale", 1.0),
-        getParam<double>(ns+"/cam_fx"),
-        getParam<double>(ns+"/cam_fy"),
-        getParam<double>(ns+"/cam_cx"),
-        getParam<double>(ns+"/cam_cy"),
-        getParam<double>(ns+"/cam_d0", 0.0),
-        getParam<double>(ns+"/cam_d1", 0.0),
-        getParam<double>(ns+"/cam_d2", 0.0),
-        getParam<double>(ns+"/cam_d3", 0.0));
+        cam_width, cam_height, scale,
+        cam_fx, cam_fy, cam_cx, cam_cy,
+        cam_d0, cam_d1, cam_d2, cam_d3
+      );
   }
   else if(cam_model == "EquidistantCamera")
   {
+    int cam_width = 640;
+    int cam_height = 480;
+    double scale = 1.0;
+    double cam_fx = 0.0;
+    double cam_fy = 0.0;
+    double cam_cx = 0.0;
+    double cam_cy = 0.0;
+    double k1 = 0.0;
+    double k2 = 0.0;
+    double k3 = 0.0;
+    double k4 = 0.0;
+
+    nh->get_parameter(ns + ".cam_width", cam_width);
+    nh->get_parameter(ns + ".cam_height", cam_height);
+    nh->get_parameter(ns + ".scale", scale);
+    nh->get_parameter(ns + ".cam_fx", cam_fx);
+    nh->get_parameter(ns + ".cam_fy", cam_fy);
+    nh->get_parameter(ns + ".cam_cx", cam_cx);
+    nh->get_parameter(ns + ".cam_cy", cam_cy);
+    nh->get_parameter(ns + ".k1", k1);
+    nh->get_parameter(ns + ".k2", k2);
+    nh->get_parameter(ns + ".k3", k3);
+    nh->get_parameter(ns + ".k4", k4);
+
     cam = new vk::EquidistantCamera(
-        getParam<int>(ns+"/cam_width"),
-        getParam<int>(ns+"/cam_height"),
-        getParam<double>(ns+"/scale", 1.0),
-        getParam<double>(ns+"/cam_fx"),
-        getParam<double>(ns+"/cam_fy"),
-        getParam<double>(ns+"/cam_cx"),
-        getParam<double>(ns+"/cam_cy"),
-        getParam<double>(ns+"/k1", 0.0),
-        getParam<double>(ns+"/k2", 0.0),
-        getParam<double>(ns+"/k3", 0.0),
-        getParam<double>(ns+"/k4", 0.0));
+        cam_width, cam_height, scale,
+        cam_fx, cam_fy, cam_cx, cam_cy,
+        k1, k2, k3, k4
+      );
   }
   else if(cam_model == "PolynomialCamera")
   {
+    double cam_width = 640;
+    double cam_height = 480;
+    double scale = 1.0;
+    double cam_fx = 0.0;
+    double cam_fy = 0.0;
+    double cam_cx = 0.0;
+    double cam_cy = 0.0;
+    double cam_skew;
+    double k2 = 0.0;
+    double k3 = 0.0;
+    double k4 = 0.0;
+    double k5 = 0.0;
+    double k6 = 0.0;
+    double k7 = 0.0;
+    nh->get_parameter(ns + ".cam_width", cam_width);
+    nh->get_parameter(ns + ".cam_height", cam_height);
+    // nh->get_parameter(ns + ".scale", scale);
+    nh->get_parameter(ns + ".cam_fx", cam_fx);
+    nh->get_parameter(ns + ".cam_fy", cam_fy);
+    nh->get_parameter(ns + ".cam_cx", cam_cx);
+    nh->get_parameter(ns + ".cam_cy", cam_cy);
+    nh->get_parameter(ns + ".cam_skew", cam_skew);
+    nh->get_parameter(ns + ".k2", k2);
+    nh->get_parameter(ns + ".k3", k3);
+    nh->get_parameter(ns + ".k4", k4);
+    nh->get_parameter(ns + ".k5", k5);
+    nh->get_parameter(ns + ".k6", k6);
+    nh->get_parameter(ns + ".k7", k7);
+    
     cam = new vk::PolynomialCamera(
-        getParam<int>(ns+"/cam_width"),
-        getParam<int>(ns+"/cam_height"),
-        // getParam<double>(ns+"/scale", 1.0),
-        getParam<double>(ns+"/cam_fx"),
-        getParam<double>(ns+"/cam_fy"),
-        getParam<double>(ns+"/cam_cx"),
-        getParam<double>(ns+"/cam_cy"),
-        getParam<double>(ns+"/cam_skew"),
-        getParam<double>(ns+"/k2", 0.0),
-        getParam<double>(ns+"/k3", 0.0),
-        getParam<double>(ns+"/k4", 0.0),
-        getParam<double>(ns+"/k5", 0.0),
-        getParam<double>(ns+"/k6", 0.0),
-        getParam<double>(ns+"/k7", 0.0));
+      cam_width, cam_height,
+      cam_fx, cam_fy, cam_cx, cam_cy,
+      cam_skew, k2, k3, k4, k5, k6, k7
+    );
   }
   else if(cam_model == "ATAN")
   {
+    int cam_width = 640;
+    int cam_height = 480;
+    double cam_fx = 0.0;
+    double cam_fy = 0.0;
+    double cam_cx = 0.0;
+    double cam_cy = 0.0;
+    double cam_d0 = 0.0;
+
+    nh->get_parameter(ns + ".cam_width", cam_width);
+    nh->get_parameter(ns + ".cam_height", cam_height);
+    nh->get_parameter(ns + ".cam_fx", cam_fx);
+    nh->get_parameter(ns + ".cam_fy", cam_fy);
+    nh->get_parameter(ns + ".cam_cx", cam_cx);
+    nh->get_parameter(ns + ".cam_cy", cam_cy);
+    nh->get_parameter(ns + ".cam_d0", cam_d0);
+
     cam = new vk::ATANCamera(
-        getParam<int>(ns+"/cam_width"),
-        getParam<int>(ns+"/cam_height"),
-        getParam<double>(ns+"/cam_fx"),
-        getParam<double>(ns+"/cam_fy"),
-        getParam<double>(ns+"/cam_cx"),
-        getParam<double>(ns+"/cam_cy"),
-        getParam<double>(ns+"/cam_d0"));
+      cam_width, cam_height,
+      cam_fx, cam_fy, cam_cx, cam_cy,
+      cam_d0
+    );
   }
   else
   {
@@ -96,52 +168,24 @@ bool loadFromRosNs(const std::string& ns, vk::AbstractCamera*& cam)
   return res;
 }
 
-bool loadFromRosNs(const std::string& ns, std::vector<vk::AbstractCamera*>& cam_list)
+bool loadFromRosNs(rclcpp::Node::SharedPtr nh, const std::string& ns, std::vector<vk::AbstractCamera*>& cam_list)
 {
   bool res = true;
-  std::string cam_model(getParam<std::string>(ns+"/cam_model"));
-  int cam_num = getParam<int>(ns+"/cam_num");
-  for (int i = 0; i < cam_num; i ++)
-  {
-    std::string cam_ns = ns + "/cam_" + std::to_string(i);
-    std::string cam_model(getParam<std::string>(cam_ns+"/cam_model"));
-    if(cam_model == "FishPoly")
-    {
-      cam_list.push_back(new vk::PolynomialCamera(
-        getParam<int>(cam_ns+"/image_width"),
-        getParam<int>(cam_ns+"/image_height"),
-        // getParam<double>(cam_ns+"/scale", 1.0),
-        getParam<double>(cam_ns+"/A11"),  // cam_fx
-        getParam<double>(cam_ns+"/A22"),  // cam_fy
-        getParam<double>(cam_ns+"/u0"),  // cam_cx
-        getParam<double>(cam_ns+"/v0"),  // cam_cy
-        getParam<double>(cam_ns+"/A12"), // cam_skew
-        getParam<double>(cam_ns+"/k2", 0.0),
-        getParam<double>(cam_ns+"/k3", 0.0),
-        getParam<double>(cam_ns+"/k4", 0.0),
-        getParam<double>(cam_ns+"/k5", 0.0),
-        getParam<double>(cam_ns+"/k6", 0.0),
-        getParam<double>(cam_ns+"/k7", 0.0)));
-    }
-    else if(cam_model == "Pinhole")
-    {
-      cam_list.push_back(new vk::PinholeCamera(
-          getParam<int>(ns+"/cam_width"),
-          getParam<int>(ns+"/cam_height"),
-          getParam<double>(ns+"/scale", 1.0),
-          getParam<double>(ns+"/cam_fx"),
-          getParam<double>(ns+"/cam_fy"),
-          getParam<double>(ns+"/cam_cx"),
-          getParam<double>(ns+"/cam_cy"),
-          getParam<double>(ns+"/cam_d0", 0.0),
-          getParam<double>(ns+"/cam_d1", 0.0),
-          getParam<double>(ns+"/cam_d2", 0.0),
-          getParam<double>(ns+"/cam_d3", 0.0)));
-    }
-    else 
-    {
-      // cam_list.clear();
+  int cam_num;
+  if (!nh->get_parameter(ns + ".cam_num", cam_num)) {
+    RCLCPP_ERROR(nh->get_logger(), "Camera number parameter not found: %s.cam_num", ns.c_str());
+    return false;
+  }
+
+  for (int i = 0; i < cam_num; i++) {
+    std::string cam_ns = ns + ".cam_" + std::to_string(i);
+    vk::AbstractCamera *cam;
+    
+    if (!loadFromRosNs(nh, cam_ns, cam)) {
+      RCLCPP_WARN(nh->get_logger(), "Failed to load camera from namespace: %s", cam_ns.c_str());
       res = false;
+    } else {
+      cam_list.push_back(cam);
     }
   }
   
--- vikit_ros/include/vikit/output_helper.h
+++ vikit_ros/include/vikit/output_helper.h
@@ -9,10 +9,12 @@
 #define VIKIT_OUTPUT_HELPER_H_
 
 #include <string>
-#include <ros/ros.h>
+#include <rclcpp/rclcpp.hpp>
 #include <Eigen/Core>
 #include <sophus/se3.h>
-#include <tf/transform_broadcaster.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <visualization_msgs/msg/marker.hpp>
 
 namespace vk {
 namespace output_helper {
@@ -21,73 +23,73 @@ using namespace std;
 using namespace Eigen;
 
 void
-publishTfTransform      (const Sophus::SE3& T, const ros::Time& stamp,
+publishTfTransform      (const Sophus::SE3& T, const rclcpp::Time& stamp,
                          const string& frame_id, const string& child_frame_id,
-                         tf::TransformBroadcaster& br);
+                         tf2_ros::TransformBroadcaster& br);
 
 void
-publishPointMarker      (ros::Publisher pub,
+publishPointMarker      (rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr pub,
                          const Vector3d& pos,
                          const string& ns,
-                         const ros::Time& timestamp,
+                         const rclcpp::Time& timestamp,
                          int id,
                          int action,
                          double marker_scale,
                          const Vector3d& color,
-                         ros::Duration lifetime = ros::Duration(0.0));
+                         rclcpp::Duration lifetime = rclcpp::Duration(0, 0));
 
 void
-publishLineMarker       (ros::Publisher pub,
+publishLineMarker       (rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr pub,
                          const Vector3d& start,
                          const Vector3d& end,
                          const string& ns,
-                         const ros::Time& timestamp,
+                         const rclcpp::Time& timestamp,
                          int id,
                          int action,
                          double marker_scale,
                          const Vector3d& color,
-                         ros::Duration lifetime = ros::Duration(0.0));
+                         rclcpp::Duration lifetime = rclcpp::Duration(0, 0));
 
 void
-publishArrowMarker      (ros::Publisher pub,
+publishArrowMarker      (rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr pub,
                          const Vector3d& pos,
                          const Vector3d& dir,
                          double scale,
                          const string& ns,
-                         const ros::Time& timestamp,
+                         const rclcpp::Time& timestamp,
                          int id,
                          int action,
                          double marker_scale,
                          const Vector3d& color);
 
 void
-publishHexacopterMarker (ros::Publisher pub,
+publishHexacopterMarker (rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr pub,
                          const string& frame_id,
                          const string& ns,
-                         const ros::Time& timestamp,
+                         const rclcpp::Time& timestamp,
                          int id,
                          int action,
                          double marker_scale,
                          const Vector3d& color);
 
 void
-publishCameraMarker(ros::Publisher pub,
+publishCameraMarker(rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr pub,
                     const string& frame_id,
                     const string& ns,
-                    const ros::Time& timestamp,
+                    const rclcpp::Time& timestamp,
                     int id,
                     double marker_scale,
                     const Vector3d& color);
 void
-publishFrameMarker     (ros::Publisher pub,
+publishFrameMarker     (rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr pub,
                         const Matrix3d& rot,
                         const Vector3d& pos,
                         const string& ns,
-                        const ros::Time& timestamp,
+                        const rclcpp::Time& timestamp,
                         int id,
                         int action,
                         double marker_scale,
-                        ros::Duration lifetime = ros::Duration(0.0));
+                        rclcpp::Duration lifetime = rclcpp::Duration(0, 0));
 
 
 } // namespace output_helper
